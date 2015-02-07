9566add927d06d90630c4daae19880cd8dbee548
Selami <selamialtin@gmail.com>
2015-02-07 16:37:12 +0200

    first

---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,2 @@
+//jasper.dir.reports = "/grails-1.0.2/workspace/Jasper/web-app/reports"
+//jasper.dir.reports = "reports"
---
+++ src/groovy/org/codehaus/groovy/grails/plugins/jasper/JasperReportParameter.groovy
@@ -0,0 +1,16 @@
+package org.codehaus.groovy.grails.plugins.jasper
+
+class JasperReportParameter {
+
+	String name
+	
+	String description
+	
+	Class<?> valueClass
+
+	String valueClassName
+	
+	Class<?> nestedType
+	
+	String nestedTypeName
+}
